9a69308ab4fef47633fa7cd72e0beb2f050071dc
Okuda-Takuya-0223 <tamiwomax@gmail.com>
2025-01-13 22:46:50 +0900

    route created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/calc/{number1)/{calculate}/{number2}', [
+    MessageController::class,
+    'cal'
+]);
